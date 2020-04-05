a9a26ee55a17879d744f998227787106e76d8ddc
Mikhail Keselman <m.kes@skbkontur.ru>
2020-04-05 14:51:26 +0500

    fix port

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.use(express.static("./"))
+app.use(express.static("./"));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(80, function() {
+  console.log("Example app listening on port 3000!");
 });
